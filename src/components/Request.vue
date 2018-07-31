c15021ff339bfb27120dbcd6452d899c3f05fa57
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2018-08-01 09:00:45 +1000

    More styling of request element

--- src/components/Request.vue
+++ src/components/Request.vue
@@ -1,13 +1,15 @@
 <template>
   <form @submit.prevent="submit">
-    <label>
-       Domain:
-      <input type="text" v-model="domain"/>
-    </label>
-    <label>
-       Path:
-      <input type="text" v-model="path"/>
-    </label>
+    <div class="row">
+      <div class="align-right column">
+        <label>DOMAIN</label>
+        <input class="align-right" type="text" v-model="domain"/>
+      </div>
+      <div id="path" class="column">
+        <label>PATH</label>
+        <input type="text" v-model="path"/>
+      </div>
+    </div>
     <button type="submit">Submit</button>
   </form>
 </template>
@@ -29,16 +31,55 @@ export default Vue.extend({
             const fetch: Fetch = new Fetch(this.domain);
             const response: string = await fetch.get(this.path);
 
-            this.$store.commit('updateresponse', response);
+            this.$store.commit('updateResponse', response);
         },
     },
 });
 </script>
 
 <style scoped>
-input[type=text] {
-    width: %100;
-    padding: 12px 20px;
-    margin-top: 60px;
+form {
+  margin: 10px 0;
 }
+  
+.row {
+  display: flex;
+  flex-direction: row;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+}
+
+.align-right {
+  text-align: right;
+}
+
+#path {
+  width: 60%;
+}
+
+label {
+  flex: 1 1 auto;
+  font-size: 0.8em;
+  margin: 0 6px;
+}
+
+input {
+  margin: 2px 0;
+  background-color: #073642;
+  color: #839496;
+  border: 1pt solid #586e75;
+}
+
+button {
+  margin: 10px 0;
+  float: right;
+  background-color: #073642;
+  color: #839496;
+  border: 1pt solid #586e75;
+}
+
 </style>
